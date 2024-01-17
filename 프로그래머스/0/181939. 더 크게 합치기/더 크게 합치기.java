fff8f7a1719afc93c1c0accd3c96fd1d12e53e54
Sunghoon Lee <78409405+ssunghoon@users.noreply.github.com>
2024-01-18 00:35:59 +0900

    [level 0] Title: 더 크게 합치기, Time: 1.29 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -2,7 +2,8 @@ class Solution {
     public int solution(int a, int b) {
         String A = String.valueOf(a);
         String B = String.valueOf(b);
-        int answer = Integer.parseInt(A+B) > Integer.parseInt(B+A) ? Integer.parseInt(A+B) : Integer.parseInt(B+A) ;
+        int answer = Integer.parseInt(A + B) > Integer.parseInt(B + A) ? Integer.parseInt(A + B)
+                : Integer.parseInt(B + A);
         return answer;
     }
 }
