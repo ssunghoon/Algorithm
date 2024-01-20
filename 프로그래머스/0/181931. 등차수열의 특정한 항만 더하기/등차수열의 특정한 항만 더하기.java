60fa2eea7656068f03a10cf6c23a93531b988591
Sunghoon Lee <78409405+ssunghoon@users.noreply.github.com>
2024-01-20 22:50:57 +0900

    [level 0] Title: 등차수열의 특정한 항만 더하기, Time: 0.03 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int a, int d, boolean[] included) {
+        int answer = 0;
+        int sum = 0;
+        for (int i = 0; i < included.length; i++) {
+            sum += a + (d * i);
+            if (included[i] == true) {
+                answer += sum;
+            }
+            sum = 0;
+        }
+        return answer;
+    }
+}
