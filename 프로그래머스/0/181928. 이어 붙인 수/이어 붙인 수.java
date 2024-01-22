a11ef09683e62e2121d4d04d058d301261b5173f
Sunghoon Lee <78409405+ssunghoon@users.noreply.github.com>
2024-01-23 02:45:04 +0900

    [level 0] Title: 이어 붙인 수, Time: 8.75 ms, Memory: 72.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int[] num_list) {
+        String even = "";
+        String odd = "";
+        for (int i = 0; i < num_list.length; i++) {
+            if (num_list[i] % 2 == 0) {
+                even += num_list[i] + "";
+                continue;
+            }
+            odd += num_list[i] + "";
+        }
+        return Integer.parseInt(odd) + Integer.parseInt(even);
+    }
+}
